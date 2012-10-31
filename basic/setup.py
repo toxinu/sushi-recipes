bc22da48c0bf31b7826687642b14a7b76e0454fb
Socketubs <geoffrey@lehee.name>
2012-10-31 10:55:00 +0100

    Update recipes

--- basic/setup.py
+++ basic/setup.py
@@ -9,6 +9,20 @@ try:
 except ImportError:
 	from distutils.core import setup
 
+def get_version():
+    VERSIONFILE = '{{ app|lower }}/__init__.py'
+    initfile_lines = open(VERSIONFILE, 'rt').readlines()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        if mo:
+            return mo.group(1)
+    raise RuntimeError('Unable to find version string in %s.' % (VERSIONFILE,))
+
+if sys.argv[-1] == 'publish':
+	os.system('python setup.py sdist upload')
+	sys.exit()
+
 setup(
 	name			= '{{ app|title }}',
 	version			= '0.1',
