efe090d9acc16a417bac1ddf7c4808ca6a046ae2
Socketubs <geoffrey@lehee.name>
2012-11-17 19:19:32 +0100

    Update django recipe

--- django/__app__/urls.py
+++ django/__app__/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,6 +8,5 @@ urlpatterns = patterns('',
     # url(r'^$', '{{ app|lower }}.views.home', name='home'),
     # url(r'^{{ app|lower }}/', include('{{ app|lower }}.foo.urls')),
 
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- django/__app__/wsgi.py
+++ django/__app__/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for {{ app|title }} project.
+WSGI config for {{ app|lower }} project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
